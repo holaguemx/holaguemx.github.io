479cea5734354187252ec78e3284f3fcef852805
holaguemx <holague59@gmail.com>
2025-08-12 18:21:13 -0600

    Update sources.js

--- modules/sources.js
+++ modules/sources.js
@@ -1,14 +1,9 @@
-const SHEET_PINS1_URL =
-"https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5kbzFC8KW-W-nN3kHkcGRV2tjWF_bJjSe-hsfmm8JQTZ_ZsY8S6Toc-jt6RDKDGVXE3CMWGilp52T/pub?gid=101179819&single=true&output=csv";
+const SHEET_PINS1_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5kbzFC8KW-W-nN3kHkcGRV2tjWF_bJjSe-hsfmm8JQTZ_ZsY8S6Toc-jt6RDKDGVXE3CMWGilp52T/pub?gid=101179819&single=true&output=csv";
 
-const SHEET_PINS2_URL = 
-"https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5kbzFC8KW-W-nN3kHkcGRV2tjWF_bJjSe-hsfmm8JQTZ_ZsY8S6Toc-jt6RDKDGVXE3CMWGilp52T/pub?gid=928048829&single=true&output=csv";
+const SHEET_PINS2_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5kbzFC8KW-W-nN3kHkcGRV2tjWF_bJjSe-hsfmm8JQTZ_ZsY8S6Toc-jt6RDKDGVXE3CMWGilp52T/pub?gid=928048829&single=true&output=csv";
 
-const SHEET_NSE_URL = 
-"https://docs.google.com/spreadsheets/d/e/2PACX-1vR9CUmue2eopaly_vepho2L9-W1hbKCWceeQw6aTHUfhX0vVtQRcDDdaXNvCN7DgtpEKSiEvbJFb68d/pub?gid=1770754104&single=true&output=csv";
+const SHEET_NSE_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR9CUmue2eopaly_vepho2L9-W1hbKCWceeQw6aTHUfhX0vVtQRcDDdaXNvCN7DgtpEKSiEvbJFb68d/pub?gid=1770754104&single=true&output=csv";
 
-const SHEET_TRAFFIC = 
-"https://docs.google.com/spreadsheets/d/e/2PACX-1vRG8BfgGTQhABMwVhfcC1N4yWMr8cuiA03y5u9CXBpYWwuvw_XpIVafVYXvvyOCCkzWCO4BHOcGsiLR/pub?gid=0&single=true&output=csv";
+const SHEET_TRAFFIC = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRG8BfgGTQhABMwVhfcC1N4yWMr8cuiA03y5u9CXBpYWwuvw_XpIVafVYXvvyOCCkzWCO4BHOcGsiLR/pub?gid=0&single=true&output=csv";
 
-const SHEET_LOCATIONS_URL = 
-"https://docs.google.com/spreadsheets/d/e/2PACX-1vQM4pK2eOKZCYmY99fWLDRnTkqHgLOxPPbM7xX0tY58zAC00httARbYXezo46H6PTu7sSQVfkpxd1pJ/pub?gid=0&single=true&output=csv";
+const SHEET_LOCATIONS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQM4pK2eOKZCYmY99fWLDRnTkqHgLOxPPbM7xX0tY58zAC00httARbYXezo46H6PTu7sSQVfkpxd1pJ/pub?gid=0&single=true&output=csv";
